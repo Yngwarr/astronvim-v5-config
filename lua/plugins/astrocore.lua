838f930a457bab05f84ae484e97fcdb0f062110b
Yngvarr <k.yngvarr@gmail.com>
2025-07-01 19:27:06 +0200

    more snippets

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -44,7 +44,8 @@ return {
         sidescrolloff = 4,
         cmdheight = 1,
         foldmethod = "expr",
-        foldexpr = "nvim_treesitter#foldexpr()",
+        -- foldexpr = "nvim_treesitter#foldexpr()",
+        foldexpr = "v:lua.vim.treesitter.foldexpr()",
         foldlevel = 99,
         foldenable = false,
         -- remove the default tabline
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -281,6 +281,22 @@ return {
     },
   },
 
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      -- local luasnip = require "luasnip"
+      -- luasnip.filetype_extend("javascript", { "javascriptreact" })
+
+
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath("config") .. "/snippets" },
+      })
+
+    end,
+  },
+
   -- You can disable default plugins as follows:
   -- I have so many escape buttons on my keyboard, why would I need it?
   { "max397574/better-escape.nvim", enabled = false },
@@ -292,20 +308,11 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- until I figure out how to change underline to highlight
   { "RRethy/vim-illuminate", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
   -- too bright for my taste
   -- { "folke/todo-comments.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
-
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
